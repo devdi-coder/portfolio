18045c2e35b53ea244e3291e4accda1062746009
Diego Santos <diegosfaria022@hotmail.com>
2025-03-13 13:28:02 -0300

    Portfolio em flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('curriculo.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
